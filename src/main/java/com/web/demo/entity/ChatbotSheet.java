f5bbad5a7a8d123940907b80cacb9e125e7bdaae
jaeheehong0428 <jaeheehong0428@gmail.com>
2024-08-20 16:06:58 +0900

    Feat: 네이버 영화 리뷰 데이터 게시판 페이징
    엔티티 등등

---
+++ src/main/java/com/web/demo/entity/ChatbotSheet.java
@@ -0,0 +1,44 @@
+/**
+ * 챗봇 시트(질문에 대한 대답 메뉴얼 데이터)
+ */
+package com.web.demo.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import groovy.transform.ToString;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@lombok.ToString
+public class ChatbotSheet {
+	@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_generator6")
+	@SequenceGenerator(name = "sequence_generator6", sequenceName = "sequence_name6", allocationSize = 1)
+	  private Integer id;
+	
+	@Column(length = 2048)
+	private String question;
+
+	@Column(length = 2048)
+	private String answer;
+
+	private Integer label;
+
+
+
+
+
+
+
+}

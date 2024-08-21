b471c8496befb05b3331d1bbff81aef63281616c
jaeheehong0428 <jaeheehong0428@gmail.com>
2024-08-21 16:10:54 +0900

    Feat:0821최종

--- src/main/java/com/web/demo/controller/NaverReviewsController.java
+++ src/main/java/com/web/demo/controller/NaverReviewsController.java
@@ -1,5 +1,6 @@
 /**
- * 클라이언트에게 네이버 영화 리뷰 데이터를 게시판형태로 제공하는 컨트롤러이다.
+ * 클라이언트에게 네이버 영화 리뷰 데이터를 게시판형태로 제공
+ * 
  */
 package com.web.demo.controller;
 
@@ -20,28 +21,31 @@ public class NaverReviewsController {
 	@Autowired
 	private NaverReviewsService naverReviewsService;
 	
-//	~/bbs/list?page=1 이런 방식으로 호출
+	// ~/bbs/list?page=1 이런 방식으로 호출
 	@GetMapping("/list")
-	public String list(Model model,
-			@RequestParam(value="page", defaultValue ="0")int page){
-//		1. 특정 페이지(인자)에 해당되는 페이징 데이터 획득
-		Page<NaverReviews> paging = this.naverReviewsService.getList(page);
-//		2. html에 전달
+	public String list(Model model, 
+			@RequestParam(value="page", defaultValue = "0") int page) {
+		// 1. 특정 페이지(인자)에 해당되는 페이징 데이터 획득
+		Page<NaverReviews> paging= this.naverReviewsService.getList(page);
+		// 2. html에 전달
 		model.addAttribute("paging", paging);
 		return "naver_reviews_list";
-		
 	}
 	
-		@GetMapping("/list2")
-		public String list2(Model model, 
-				@RequestParam(value="page", defaultValue = "0") int page) {
-			// 1. 특정 페이지(인자)에 해당되는 페이징 데이터 획득
-			Page<NaverReviews> paging= this.naverReviewsService.getList(page);
-			// 2. html에 전달
-			model.addAttribute("paging", paging);
-			return "naver_reviews_list";
-		}
-
+	@GetMapping("/list2")
+	public String list2(Model model, 
+			@RequestParam(value="page", defaultValue = "0") int page) {
+		// 1. 특정 페이지(인자)에 해당되는 페이징 데이터 획득
+		Page<NaverReviews> paging= this.naverReviewsService.getList(page);
+		// 2. html에 전달
+		model.addAttribute("paging", paging);
+		return "naver_reviews_list";
+	}
 }
 
-	
+
+
+
+
+
+
